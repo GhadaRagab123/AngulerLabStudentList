05d6f87d7027e8cf8be164b97cbeaab1949da02b
GhadaRagab123 <ghadar109@gmail.com>
2021-05-10 03:45:51 +0200

    deploy on heroco 3

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/liststudent123'));
+app.use(express.static('./dist/lab-project'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/liststudent123/'}),
+    res.sendFile('index.html', {root: 'dist/lab-project/'}),
 );
 
 // Start the app by listening on the default Heroku port
